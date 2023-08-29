702719aefa37be7d4abe9014008f1a93d04a8e3f
Anindit P <86210514+Anipati2@users.noreply.github.com>
2023-08-28 19:56:04 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,108 @@
-// this is the js
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+let lastpressed = 'not';
+
+const screen = document.querySelector('.screen');
+
+function buttonClick(value){
+    if(isNaN(value)){
+        handleSymbol(value);
+
+    }else{
+        handleNumber(value);
+        screen.innerText = buffer;
+    }
+}
+
+function handleSymbol(symbol){
+    lastpressed = 'not';
+
+    switch(symbol){
+        case 'C':
+            buffer = '0';
+            runningTotal = 0;
+            screen.innerText = buffer;
+            break;
+        case '=':
+            if(previousOperator === null){
+                return
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            // buffer = runningTotal = 0;
+            screen.innerText = buffer;
+            lastpressed = 'equal';
+            break;
+        case '←':
+            if(buffer.length ===1){
+                buffer = '0';
+
+            }else{
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+            screen.innerText = buffer;
+            break;
+        case '+':
+        case '−':
+        case '×':
+        case '÷':
+            handleMath(symbol);
+            break;
+
+    }
+}
+
+function handleMath(symbol){
+    if(buffer === '0'){
+        return
+    }
+
+    const intBuffer = parseInt(buffer);
+
+    if(runningTotal === 0){
+        runningTotal = intBuffer;
+
+    }else{
+        flushOperation(intBuffer);
+    }
+    previousOperator = symbol;
+    buffer = '0';
+
+}
+
+function flushOperation(intBuffer){
+    if(previousOperator === '+'){
+        runningTotal += intBuffer;
+
+    }else if(previousOperator === '−'){
+        runningTotal -= intBuffer;
+
+    }else if(previousOperator === '×'){
+        runningTotal *= intBuffer
+
+    }else if(previousOperator === '÷'){
+        runningTotal /= intBuffer;
+    }
+    // alert(runningTotal);
+    buffer = runningTotal;
+}
+
+function handleNumber(numberString){
+    if(buffer === "0" || lastpressed == "equal"){
+        buffer = numberString;
+
+    }else{
+        buffer += numberString;
+    }
+    lastpressed = 'not';
+
+}
+
+function init(){
+    document.querySelector('.calc-buttons').addEventListener('click', function(event){
+       buttonClick(event.target.innerText); 
+    })
+}
+
+init();
